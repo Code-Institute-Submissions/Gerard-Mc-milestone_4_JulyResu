d050ccae7d0531c865c153a872bf732aed6fac66
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-07-27 22:36:18 +0000

    Send confirm email to user & create order after error.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
-
-from .models import Order, OrderLineItem
-from checkout.models import Category
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from decimal import Decimal
+from .models import Order, OrderLineItem, Category
+from profile_page.models import UserProfile
 
 import json
 import time
@@ -12,6 +16,23 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
 
     def handle_event(self, event):
         """
@@ -31,30 +52,28 @@ class StripeWH_Handler:
         save_info = intent.metadata.save_info
 
         billing_details = intent.charges.data[0].billing_details
-        shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
 
-        # Clean data in the shipping details
-        for field, value in shipping_details.address.items():
-            if value == "":
-                shipping_details.address[field] = None
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info == "True":
+                profile.default_phone_number = billing_details.phone
+                profile.default_email = billing_details.email
+                profile.default_country = billing_details.address.country
+                profile.default_postcode = billing_details.address.postal_code
+                profile.default_town_or_city = billing_details.address.city
+                profile.default_street_address1 = billing_details.address.line1
+                profile.default_street_address2 = billing_details.address.line2
+                profile.default_county = billing_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
         while attempt <= 5:
             try:
                 order = Order.objects.get(
-                    full_name__iexact=shipping_details.name,
-                    email__iexact=billing_details.email,
-                    phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
-                    town_or_city__iexact=shipping_details.address.city,
-                    street_address1__iexact=shipping_details.address.line1,
-                    street_address2__iexact=shipping_details.address.line2,
-                    county__iexact=shipping_details.address.state,
-                    grand_total=grand_total,
-                    original_cart=cart,
                     stripe_pid=pid,
                 )
                 order_exists = True
@@ -63,48 +82,65 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                content=(f'Webhook received: {event["type"]} | SUCCESS: '
+                         'Verified order already in database'),
                 status=200)
         else:
             order = None
             try:
                 order = Order.objects.create(
-                    full_name=shipping_details.name,
+                    full_name=billing_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
-                    phone_number=shipping_details.phone,
-                    country=shipping_details.address.country,
-                    postcode=shipping_details.address.postal_code,
-                    town_or_city=shipping_details.address.city,
-                    street_address1=shipping_details.address.line1,
-                    street_address2=shipping_details.address.line2,
-                    county=shipping_details.address.state,
+                    phone_number=billing_details.phone,
+                    country=billing_details.address.country,
+                    postcode=billing_details.address.postal_code,
+                    town_or_city=billing_details.address.city,
+                    street_address1=billing_details.address.line1,
+                    street_address2=billing_details.address.line2,
+                    county=billing_details.address.state,
                     original_cart=cart,
                     stripe_pid=pid,
                 )
-                for item_id, item_data in json.loads(cart).items():
-                    category = get_object_or_404(Category, pk=int(item[1]["category"]))
+                for item in json.loads(cart).items():
+                    item_price = 0
+                    complexity = Decimal(item[1]["complexity"])
+                    variations = Decimal(item[1]["variations"])
+                    category = get_object_or_404(
+                        Category, pk=int(item[1]["category"]))
+                    item_price = category.price
+                    item_price = Decimal(item_price) * complexity
+                    item_price = Decimal(item_price) * variations
+                    if item[1]["fast_delivery"] == "True":
+                        item_price = Decimal(item_price) * Decimal(
+                            settings.FAST_DELIVERY_CHARGE)
+                        item_price = Decimal(round(item_price, 2))
+
                     order_line_item = OrderLineItem(
                         order=order,
                         category=category,
                         complexity=item[1]["complexity"],
                         variations=item[1]["variations"],
                         user_description=item[1]["user_description"],
-                        fast_delivery=delivery,
+                        fast_delivery=item[1]["fast_delivery"],
                         lineitem_total=Decimal(item_price),
-                    )
-                       
+                        )
+                    order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
+        self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            content=(f'Webhook received: {event["type"]} | SUCCESS: '
+                     'Created order in webhook'),
             status=200)
 
-    def handle_payment_intent_payment_failed(self, event):
+    def handle_payment_intent_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
         """
