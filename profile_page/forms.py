bef9ec87a3b1c1191d3fa58160ceb9d7252e13f9
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-07-27 17:57:37 +0000

    Add field to UserProfile model and update UserProfileForm

--- profile_page/forms.py
+++ profile_page/forms.py
@@ -5,7 +5,11 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        exclude = ('user',)
+        fields = (
+            'default_phone_number', 'default_email', 'default_postcode',
+            'default_town_or_city', 'default_street_address1',
+            'default_street_address2', 'default_country',
+            'default_county')
 
     def __init__(self, *args, **kwargs):
         """
@@ -13,22 +17,15 @@ class UserProfileForm(forms.ModelForm):
         labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
-        placeholders = {
-            'default_phone_number': 'Phone Number',
-            'default_postcode': 'Postal Code',
-            'default_town_or_city': 'Town or City',
-            'default_street_address1': 'Street Address 1',
-            'default_street_address2': 'Street Address 2',
-            'default_county': 'County, State or Locality',
-        }
-
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'default_country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['id'] = field
             self.fields[field].widget.attrs['class'] = 'w-100 mb-2'
-            self.fields[field].label = False
+        self.fields['default_phone_number'].label = "Phone Number"
+        self.fields['default_email'].label = "Email"
+        self.fields['default_postcode'].label = "Postcode"
+        self.fields['default_town_or_city'].label = "Town or City"
+        self.fields['default_street_address1'].label = "Street Address 1"
+        self.fields['default_street_address2'].label = "Street Address 2"
+        self.fields['default_country'].label = "Country"
+        self.fields['default_county'].label = "County"
