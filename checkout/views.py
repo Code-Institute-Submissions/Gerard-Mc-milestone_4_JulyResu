a6fa0b3b618e6251f33294ecc272fef8e431016e
Gerard-Mc <gerardmcdaid88@gmail.com>
2021-07-27 22:21:45 +0000

    Make Checkout_success save billing info to user profile

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,7 @@ from checkout.models import Category
 from profile_page.models import UserProfile
 from .models import Order, OrderLineItem
 from decimal import Decimal
+from profile_page.forms import UserProfileForm
 import json
 
 
@@ -184,9 +185,34 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_email': order.email,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Order successful! \
+        Your order number is <strong>{order_number}</strong>. A confirmation \
+        email will be sent to <strong>{order.email}</strong>.')
 
     if 'cart' in request.session:
         del request.session['cart']
